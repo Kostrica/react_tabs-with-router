937373d55485ffedbaddd14cd9efb6b07408a4f0
Kostrica <romiorw@ukr.net>
2021-09-23 00:24:25 +0300

    Updates

---
+++ precache-manifest.02138ace9672bc765d6777b0b26c09db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9fc97b9b31ddba2032b44d43ddec24",
+    "url": "./index.html"
+  },
+  {
+    "revision": "068b75caf65f84b72d83",
+    "url": "./static/css/main.ebb51dee.chunk.css"
+  },
+  {
+    "revision": "ed7ab734921cc47b40e3",
+    "url": "./static/js/2.32c6a4cd.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.32c6a4cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068b75caf65f84b72d83",
+    "url": "./static/js/main.0b46d433.chunk.js"
+  },
+  {
+    "revision": "12bee12ab4b288faf1d5",
+    "url": "./static/js/runtime-main.108729f7.js"
+  }
+]);
